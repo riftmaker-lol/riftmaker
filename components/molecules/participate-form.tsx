fbb2ba96a52d3aa4cec9d262d3a6a928f536d80f
Anas Mazouni <madadj4@gmail.com>
2023-12-02 15:26:53 +0100

    fix: allow summoner name

--- components/molecules/participate-form.tsx
+++ components/molecules/participate-form.tsx
@@ -15,9 +15,9 @@ import { Label } from '@radix-ui/react-label';
 import { useState } from 'react';
 
 const formSchema = z.object({
-  riotId: z.custom<`${string}#${string}` | ''>((val) => {
-    return typeof val === 'string' && val.length > 0 ? val.includes('#') : false;
-  }, "A sidi, riot ID dialek, machi summoner name e.g: 'sidi#1234'"),
+  riotId: z.custom<`${string}#${string}` | 'string'>((val) => {
+    return typeof val === 'string' && val.length > 0;
+  }, "Riot ID can't be empty"),
 });
 
 interface ParticipateFormProps {
@@ -61,7 +61,7 @@ const ParticipateForm = ({ tournament, session }: ParticipateFormProps) => {
 
   return (
     <div className="flex flex-col gap-2">
-      <Label className="font-bold">Riot ID</Label>
+      <Label className="font-bold">Riot ID or Summoner Name:</Label>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="items-center gap-4 flex-row flex">
           <FormField
@@ -70,7 +70,7 @@ const ParticipateForm = ({ tournament, session }: ParticipateFormProps) => {
             render={({ field }) => (
               <FormItem className="w-full">
                 <FormControl>
-                  <Input placeholder="Stormix#lurk" {...field} readOnly={joined} />
+                  <Input placeholder="Hassan#1234" {...field} readOnly={joined} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -100,6 +100,10 @@ const ParticipateForm = ({ tournament, session }: ParticipateFormProps) => {
             <a href="https://mobalytics.gg/lol" className="text-yellow-500 ml-2">
               https://mobalytics.gg/lol
             </a>
+          </li>
+
+          <li>
+            First try your riot ID: <b>Hassan#123</b>, if that doesn&apos;t work, try your summoner name: <b>Hassan</b>.
           </li>
         </ul>
       </Form>
--- lib/riot.ts
+++ lib/riot.ts
@@ -2,7 +2,6 @@ import { SummonerData } from '@/types/riot';
 import { PlayerRole } from '@prisma/client';
 
 export const summonerData = async (summonerName: string, server: string) => {
-  summonerName = decodeURIComponent(summonerName);
   const res = await fetch('https://app.mobalytics.gg/api/lol/graphql/v1/query', {
     headers: {
       accept: '*/*',
@@ -17,7 +16,7 @@ export const summonerData = async (summonerName: string, server: string) => {
       'x-moba-client': 'mobalytics-web',
       'x-moba-proxy-gql-ops-name': 'LolProfilePageSummonerInfoQuery',
     },
-    referrer: `https://app.mobalytics.gg/lol/profile/${server}/${summonerName}/overview`,
+    referrer: `https://app.mobalytics.gg/lol/profile/${server}/${encodeURIComponent(summonerName)}/overview`,
     referrerPolicy: 'strict-origin-when-cross-origin',
     body: `{"operationName":"LolProfilePageSummonerInfoQuery","variables":{"summonerName":"${summonerName}","region":"${server}","sQueue":null,"sRole":null,"sChampion":null},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"8ad5d73cd5306b9f5b423a0285e4a078976f125e0424b227e5f244af50954da7"}}}`,
     method: 'POST',
