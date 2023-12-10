12a984c6056ef44397e6f375e89242b8a09edb16
Anas Mazouni <madadj4@gmail.com>
2023-12-10 15:32:56 +0100

    feat: discord setup command

--- components/molecules/invite-link.tsx
+++ components/molecules/invite-link.tsx
@@ -10,27 +10,48 @@ import { Input } from '../ui/input';
 
 const InviteLink = ({ tournament }: { tournament: Tournament }) => {
   const inviteLink = `${env.NEXT_PUBLIC_BASE_URL}/tournament/${tournament.id}`;
+  const setupCommand = `/setup tournament:${tournament.id}`;
 
   const { toast } = useToast();
   const { copy } = useCopyToClipboard();
 
   return (
-    <div className="flex flex-col gap-4">
-      <h3 className="text-3xl font-semibold">Invite link:</h3>
-      <div className="flex flex-row gap-4">
-        <Input value={inviteLink} readOnly />
-        <Button
-          variant={'outline'}
-          onClick={() => {
-            copy(inviteLink);
-            toast({
-              description: 'Copied invite link to clipboard',
-            });
-          }}
-        >
-          <FaRegCopy className="w-4 h-4 mr-2" title="Copy invite link" />
-          Copy link
-        </Button>
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-4">
+        <h3 className="text-3xl font-semibold">Invite link:</h3>
+        <div className="flex flex-row gap-4">
+          <Input value={inviteLink} readOnly />
+          <Button
+            variant={'outline'}
+            onClick={() => {
+              copy(inviteLink);
+              toast({
+                description: 'Copied invite link to clipboard',
+              });
+            }}
+          >
+            <FaRegCopy className="w-4 h-4 mr-2" title="Copy invite link" />
+            Copy link
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h3 className="text-3xl font-semibold">Discord command:</h3>
+        <div className="flex flex-row gap-4">
+          <Input value={setupCommand} readOnly />
+          <Button
+            variant={'outline'}
+            onClick={() => {
+              copy(setupCommand);
+              toast({
+                description: 'Copied setup command to clipboard',
+              });
+            }}
+          >
+            <FaRegCopy className="w-4 h-4 mr-2" title="Copy command" />
+            Copy command
+          </Button>
+        </div>
       </div>
     </div>
   );
